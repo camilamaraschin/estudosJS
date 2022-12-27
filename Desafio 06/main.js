14dd9cad8cadfd0542469ad09e39341a3e8d6927
Camila Maraschin <105385268+camilamaraschin@users.noreply.github.com>
2022-12-27 06:39:08 -0300

    Add files via upload

---
+++ Desafio 06/main.js
@@ -0,0 +1,87 @@
+// Declarar uma variavel de nome weight 
+
+var weight 
+
+// Que tipo de dado é a variavel a cima ?
+
+console.log (typeof weight) = undefined
+
+
+// Declare uma variavel que atribua valores para cada um dos dados : 
+
+var name = Camila
+var age = 27
+var stars = 27.5
+ var isSubscibed = true
+
+ // A variavel student abaixo é de que tipo de dados?
+ object
+
+ // Atribua a ela as mesma propriedades e valores  do exercicio 3 
+
+//Mostre no console.log a seguinte mensagem :  <name> de idade <age> pesa <weight> kg
+
+let students = {
+
+    name: 'Camila',
+    age: 27,
+    weight: 80
+}
+
+console.log(`${student.name} de idade ${student.age} pesa ${student.weight} KG `)
+
+// Declare uma variavel de tipo ARRAY de nome students e atribua a ela nenhum valor,
+//ou seja somente array vazio 
+
+var students = []
+
+// Retribua valor para a variavel a cima colocando dentro dela  o objeto student 
+
+
+students =[
+    student
+]
+
+console.log (students)
+
+
+
+
+// Coloque no console o valor da posição zero do array acima
+
+var students = [
+    {
+        name: 'Camila',
+        age: 27,
+        weight: 80
+    }
+]
+
+console.log ( students[0])
+
+
+// Crie um novo student e coloque na posiçao 1 do array studentes 
+var students = [
+    {
+        name: 'Camila',
+        age: 27,
+        weight: 80
+    },
+
+    {
+        name: 'Marta',
+        age: 22,
+        weight: 89
+
+    }
+]
+
+console.log ( students[1])
+
+
+//Sem rodar o codigo qual é a resposta do codigo abaixo e porque? 
+
+console.log (a)
+var a = 1
+
+Vai imprimir um undefined pois o fez o hosting e não foi declarado antes 
