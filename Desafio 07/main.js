864120ac5ada1e3974e73bbd255b547171c402b1
Camila Maraschin <105385268+camilamaraschin@users.noreply.github.com>
2022-12-29 08:55:16 -0300

    Add files via upload

---
+++ Desafio 07/main.js
@@ -0,0 +1,12 @@
+const sum = function(){
+
+    total = n1 + n2
+    return total 
+}
+
+let n1 =89
+let n2= 87
+
+console.log(` numero 1 é ${n1}`)
+console.log(` numero 2 é ${n2}`)
+console.log(` a soma é ${sum (n1+n2)}`)
